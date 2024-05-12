37024492416677eb33efc1815e227e70c55d017a
golamrabbi151 <rabbi15-242@diu.edu.bd>
2024-05-13 00:04:01 +0600

    basic start|

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+function App() {
+  return <h1>Hello React!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
